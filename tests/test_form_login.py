56ad5d79f07b7533ac73820c732423e27f6c4f58
alennester <alennester@bk.ru>
2024-05-14 00:04:59 +0300

    commit def test_form_login

--- tests/test_form_login.py
+++ tests/test_form_login.py
@@ -6,8 +6,19 @@ from selenium.webdriver.common.by import By
 
 
 class TestFormPageLogin:
+    def test_login(self, driver):
+        login_href = FormPageLogin(driver, 'http://localhost:5000')
+        login_href.open()
+        assert driver.find_element(By.XPATH, '//*[@id="navbarMenuHeroA"]/div/a[2]').is_displayed()
+
     def test_form(self, driver):
         form_page = FormPageLogin(driver, 'http://localhost:5000/login')
         form_page.open()
         form_page.fill_fields_and_login()
         assert 'Profile' == driver.find_element(By.XPATH, '//*[@id="navbarMenuHeroA"]/div/a[2]').text
+    def test_form_login(self, driver):
+        form_page = FormPageLogin(driver, 'http://localhost:5000/login')
+        form_page.open()
+        form_page.fill_field_and_login()
+        assert 'Login' == driver.find_element(By.XPATH, '/html/body/section/div[2]/div/div/h3').text
+        time.sleep(5)
