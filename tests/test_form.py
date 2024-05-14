587adbe0d65a065d9fa9acefeafcfa25b49ab323
alennester <alennester@bk.ru>
2024-05-15 00:24:41 +0300

    commit add tests data

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,4 +1,3 @@
-import time
 import pytest
 from pages.form_page import FormPageSignUp
 from conftest import driver
@@ -18,11 +17,11 @@ class TestFormPage:
     @pytest.mark.parametrize(
         'creds',
         [
-            pytest.param(('wx@daymail.site','', '1234'), id ='fv28rk@daymail.site,,1234'),
-            # pytest.param(('sporn@yourfastmail.online','Anna', 'aaabbbccc'), id='f5u27wd@daymail.site,Анна,aaabbbccc'),
-            # pytest.param(('', '', ''), id=',,'),
-            # pytest.param(('', '', ''), id=',,'),
-            # pytest.param(('', '', ''), id=',,')
+            pytest.param(('w3m72z@yourfastmail.online','Anna', 'anna123'), id = 'w3m72z@yourfastmail.online, Anna, anna123'),
+            pytest.param(('vcrbb6@yourfastmail.online','', '123$!abs'), id= 'vcrbb6@yourfastmail.online, ,123$!abs'),
+            pytest.param(('absd12dfsgjcljahccnbvbvxv@yourfastmail.online', 'Ан', '!&ikfsdvnd'), id='absd12dfsgjcljahccnbvbvxv@yourfastmail.online, Ан, !&ikfsdvnd'),
+            pytest.param(('jva23@daymail.site', 'Nana', 'tg0EofHyC'), id='jva23@daymail.site, Nana, tg0EofHyC'),
+            pytest.param(('mareteg667@rencr.com', 'liandra', '%eN1c49#7r$%E405'), id='mareteg667@rencr.com, liandra, %eN1c49#7r$%E405')
 
         ]
     )
@@ -33,11 +32,17 @@ class TestFormPage:
     @pytest.mark.parametrize(
         'cred',
         [
-            pytest.param(('fv28rk@daymail.site','', '1234'), id ='fv28rk@daymail.site,,1234'),
-            pytest.param(('f5u27wd@daymail.site','NE', 'aaabbbccc'), id='f5u27wd@daymail.site,Анна,aaabbbccc'),
-            # pytest.param(('', '', ''), id=',,'),
-            # pytest.param(('', '', ''), id=',,'),
-            # pytest.param(('', '', ''), id=',,')
+            pytest.param(('fv28rk@daymailsite','Andre', '5oe$%s#j96D$61ey'), id ='ffv28rk@daymailsite, Andre, 5oe$%s#j96D$61ey'),
+            pytest.param(('f5u27wddaymail.site','NE', 'aaabbbccc'), id='f5u27wddaymail.site,NE , aaabbbccc'),
+            pytest.param(('', '', ''), id='empty fields'),
+            pytest.param(('hdbvp@.site', '', '123654a'), id='hdbvp@.site, 123654a'),
+            pytest.param(('hdbvp@daymail.сит', '', '@oFcr84C~ljp@V9QssI'), id='hdbvp@daymail.сит, @oFcr84C~ljp@V9QssI'),
+            pytest.param(('hmuz6m@yourfastmail.online', 'Елена', 'рвздватри'), id='hmuz6m@yourfastmail.online, Елена, рвздватри'),
+            pytest.param(('QVHJFv1@yourfastmail.online', '', '@oFcr84C'), id='QVHJFv1@yourfastmail.online, @oFcr84C'),
+            pytest.param(('FEZ@DAYMAIL.SITE', 'Lena', 'WTc6aTY7'), id='FEZ@DAYMAIL.SITE, Lena, WTc6aTY7'),
+            pytest.param(('hdbvp@.site', '', '123654a'), id='hdbvp@.site, 123654a'),
+            pytest.param(('hdbvp@фтлврс.site', '', 'hksndcbf'), id='hdbvp@фтлврс.site, hksndcbf')
+
 
         ]
     )
@@ -48,11 +53,11 @@ class TestFormPage:
     @pytest.mark.parametrize(
         'cred',
         [
-            pytest.param(('fv28rk@daymail.site', '', '1234'), id='fv28rk@daymail.site,,1234'),
-            # pytest.param(('f5u27wd@daymail.site', 'NE', 'aaabbbccc'), id='f5u27wd@daymail.site,Анна,aaabbbccc'),
-            # pytest.param(('', '', ''), id=',,'),
-            # pytest.param(('', '', ''), id=',,'),
-            # pytest.param(('', '', ''), id=',,')
+            pytest.param(('w3m72z@yourfastmail.online', 'Anna', 'anna123'), id='w3m72z@yourfastmail.online, Anna, anna123'),
+            pytest.param(('vcrbb6@yourfastmail.online', '', '123$!abs'), id='vcrbb6@yourfastmail.online, ,123$!abs'),
+            pytest.param(('absd12dfsgjcljahccnbvbvxv@yourfastmail.online', 'Ан', '!&ikfsdvnd'), id='absd12dfsgjcljahccnbvbvxv@yourfastmail.online, Ан, !&ikfsdvnd'),
+            pytest.param(('jva23@daymail.site', 'Nana', 'tg0EofHyC'), id='jva23@daymail.site, Nana, tg0EofHyC'),
+            pytest.param(('mareteg667@rencr.com', 'liandra', '%eN1c49#7r$%E405'), id='mareteg667@rencr.com, liandra, %eN1c49#7r$%E405')
 
         ]
     )
@@ -64,11 +69,14 @@ class TestFormPage:
     @pytest.mark.parametrize(
         'cred',
         [
-            pytest.param(('fv28rk@daymail.site', '', '1234'), id='fv28rk@daymail.site,,1234'),
-            # pytest.param(('f5u27wd@daymail.site', 'NE', 'aaabbbccc'), id='f5u27wd@daymail.site,Анна,aaabbbccc'),
-            # pytest.param(('', '', ''), id=',,'),
-            # pytest.param(('', '', ''), id=',,'),
-            # pytest.param(('', '', ''), id=',,')
+            pytest.param(('w3m72z@yourfastmail.online', 'Anna', 'anna123'),
+                         id='w3m72z@yourfastmail.online, Anna, anna123'),
+            pytest.param(('vcrbb6@yourfastmail.online', '', '123$!abs'), id='vcrbb6@yourfastmail.online, ,123$!abs'),
+            pytest.param(('absd12dfsgjcljahccnbvbvxv@yourfastmail.online', 'Ан', '!&ikfsdvnd'),
+                         id='absd12dfsgjcljahccnbvbvxv@yourfastmail.online, Ан, !&ikfsdvnd'),
+            pytest.param(('jva23@daymail.site', 'Nana', 'tg0EofHyC'), id='jva23@daymail.site, Nana, tg0EofHyC'),
+            pytest.param(('mareteg667@rencr.com', 'liandra', '%eN1c49#7r$%E405'),
+                         id='mareteg667@rencr.com, liandra, %eN1c49#7r$%E405')
 
         ]
     )
