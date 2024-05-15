bd25ab7de17045cced5637dc4a514bc19188dada
alennester <alennester@bk.ru>
2024-05-15 13:18:18 +0300

    commit add new def

--- pages/form_page_login.py
+++ pages/form_page_login.py
@@ -1,20 +1,55 @@
-import time
+import pytest
 from pages.base_page import BasePage
 from locators.form_page_locators import FormPageLocators as Locators
 
 
 class FormPageLogin(BasePage):
-    def fill_fields_and_login(self):
-        email = 'd47vth@yourfastmail.online'
-        password = '1234'
+    def find_login(self):
+        assert self.element_is_visible(Locators.LOGIN_HREF).is_displayed()
+
+    def login_click(self):
+        self.element_is_visible(Locators.LOGIN_HREF).click()
+        ln_cl = self.element_is_visible(Locators.LOGIN).is_displayed()
+        assert 'Login' == ln_cl
+
+    def fill_fields_and_login(self, users):
+        email, password = users
         self.element_is_visible(Locators.EMAIL_LOG).send_keys(email)
         self.element_is_visible(Locators.PASSWORD_LOG).send_keys(password)
         self.element_is_visible(Locators.LOGIN).click()
+        login = self.element_is_visible(Locators.PROFILE).text
+        assert 'Profile' == login
 
-    def fill_field_and_login(self):
-        email = 'd47vth'
-        password = '1234'
+    def fill_fields_check_login(self, users):
+        email, password = users
+        self.element_is_visible(Locators.EMAIL_LOG).send_keys(email)
+        self.element_is_visible(Locators.PASSWORD_LOG).send_keys(password)
+        self.element_is_visible(Locators.CHECK_BOX).click()
+        self.element_is_visible(Locators.LOGIN).click()
+        login = self.element_is_visible(Locators.PROFILE).text
+        assert 'Profile' == login
+
+    def fill_email_warning(self, try_again):
+        email, password = try_again
         self.element_is_visible(Locators.EMAIL_LOG).send_keys(email)
         self.element_is_visible(Locators.PASSWORD_LOG).send_keys(password)
         self.element_is_visible(Locators.LOGIN).click()
+        login = self.element_is_visible(Locators.TRY_AGAIN).text
+        assert 'Please check your login details and try again.' == login
 
+    def fill_field_and_login(self, log):
+        email, password = log
+        self.element_is_visible(Locators.EMAIL_LOG).send_keys(email)
+        self.element_is_visible(Locators.PASSWORD_LOG).send_keys(password)
+        self.element_is_visible(Locators.LOGIN).click()
+        login = self.element_is_visible(Locators.LOGIN_IN_H).text
+        assert 'Login' == login
+
+    def check_login(self, log_check):
+        email, password = log_check
+        self.element_is_visible(Locators.EMAIL_LOG).send_keys(email)
+        self.element_is_visible(Locators.PASSWORD_LOG).send_keys(password)
+        self.element_is_visible(Locators.CHECK_BOX).click()
+        self.element_is_visible(Locators.LOGIN).click()
+        login = self.element_is_visible(Locators.LOGIN_IN_H).text
+        assert 'Login' == login
